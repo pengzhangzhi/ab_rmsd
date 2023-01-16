2633961eb48b67e22df786a87be15645266eddcf
Peng Zhangzhi <pengzhangzhics@gmail.com>
2023-01-16 14:14:24 +0800

    [feat] superimpose each chain instead of the whole complex structure

--- ab_rmsd/calc_rmsd.py
+++ ab_rmsd/calc_rmsd.py
@@ -26,7 +26,6 @@ class AntibodyRMSD:
         Returns:
             dict: rmsd results of each regions (CDRH1...3, CDRL1...3)
         """
-        pred_coord_list, native_coord_list = [], []
         exist_heavy = exist_key(pred_antibody, "heavy") and exist_key(
             native_antibody, "heavy"
         )
@@ -38,58 +37,40 @@ class AntibodyRMSD:
             chain_list.append("heavy")
         if exist_light:
             chain_list.append("light")
-
+        res = {}
         for chain in chain_list:
             native_coord = native_antibody[chain]["pos_heavyatom"][
                 ..., : self.nb_atoms, :
             ]
             pred_coord = pred_antibody[chain]["pos_heavyatom"][..., : self.nb_atoms, :]
-            
-
-            pred_coord_list.append(pred_coord)
-            native_coord_list.append(native_coord)
-
-        pred_coord = torch.cat(pred_coord_list, dim=0).reshape(-1, 3)  # [nb_atoms, 3]
-        native_coord = torch.cat(native_coord_list, dim=0).reshape(
-            -1, 3
-        )  # [nb_atoms, 3]
-        rot, trans = self.kabsch_rmsd.calc_superimpose_transformation(
-            native_coord, pred_coord
-        )
-
-        if superimpose_pred:
-            for chain in chain_list:
-                coord = pred_antibody[chain]["pos_heavyatom"]
-                shape = coord.shape
-                superimposed = self.kabsch_rmsd.apply_transformation(
-                    rot, trans, coord.reshape(-1, 3)
-                )
-                pred_antibody[chain]["pos_heavyatom"] = superimposed.reshape(*shape)
-            
-        res_list = []
-        for i, chain in enumerate(chain_list):
-            pred_coord, native_coord = pred_coord_list[i], native_coord_list[i]
+            if native_coord.shape != pred_coord.shape:
+                raise ValueError(
+                    f"native coordinate mismatches pred coordinate" 
+                    f" {native_coord.shape} != {pred_coord.shape}"
+                )            
+            rot, trans = self.kabsch_rmsd.calc_superimpose_transformation(
+                native_coord.reshape(-1, 3) , pred_coord.reshape(-1, 3)
+            )
             shape = pred_coord.shape
             superimposed_pred_coord = self.kabsch_rmsd.apply_transformation(
                 rot, trans, pred_coord.reshape(-1, 3)
             )
             superimposed_pred_coord = superimposed_pred_coord.reshape(*shape)
-            chain_res = self._calc_region_rmsd(
+            
+            rmsd = self._calc_region_rmsd(
                 native_antibody[chain]["select"], superimposed_pred_coord, native_coord
             )
-            res_list.append(chain_res)
-
-        res = {}
-        for i in range(len(res_list)):
-            res.update(res_list[i])
+            res.update(rmsd)
         return res
+            
+        
 
     def _calc_region_rmsd(self, select_mask_dict, pred_coord, native_coord):
         res_dict = {}
         for region_id, region_mask in select_mask_dict.items():
             pred_region_coord = pred_coord[region_mask].reshape(-1, 3)
             native_region_coord = native_coord[region_mask].reshape(-1, 3)
-            rmsd = self.kabsch_rmsd(pred_region_coord, native_region_coord)
+            rmsd = self.kabsch_rmsd(pred_region_coord, native_region_coord,superimpose=False)
             res_dict[region_id] = rmsd
         return res_dict
 
@@ -104,15 +85,16 @@ def calc_ab_rmsd(pred_path, native_path):
     pred_ab = parse_pdb(pred_path)
     native_ab = parse_pdb(native_path)
     rmsd = AntibodyRMSD()(pred_ab,native_ab)
+    rmsd = {k:v.item() for k, v in rmsd.items()}
     return rmsd
 
-def parse_pdb(pred_path):
+def parse_pdb(pdb_path):
     """
     parse a pdb file to protein dict.
     the pdb file must contain at least the heavy chain.
     
     Args:
-        pred_path (str): 
+        pdb_path (str): 
 
     Raises:
         ValueError: No heavy chain found in pdb file.
@@ -120,11 +102,10 @@ def parse_pdb(pred_path):
     Returns:
         dict: dict of heavy and light chain info.
     """
-    model, heavy_chains, light_chains, other_chains = renumber(pred_path)
-    if len(heavy_chains) == 0: raise PDBParseError("No heavy chain found in pdb file, path: {}".format(pred_path))
+    model, heavy_chains, light_chains, other_chains = renumber(pdb_path)
+    if len(heavy_chains) == 0: raise PDBParseError("No heavy chain found in pdb file, path: {}".format(pdb_path))
     h_id = heavy_chains[0]
     l_id = light_chains[0] if len(light_chains) != 0 else None
-    
     ab_dict = preprocess_antibody_structure(
         model, h_id, l_id
     )
